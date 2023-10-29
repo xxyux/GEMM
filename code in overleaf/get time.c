ecea7483b3a5e57943b4358fa7b0d3f8423b1c68
Xiangrui Yu <61179152+xxyux@users.noreply.github.com>
2023-10-29 16:33:57 +0800

    Delete code in overleaf directory

--- code in overleaf/get time.c
+++
@@ -1,7 +0,0 @@
-gettimeofday(&start, NULL);
-while(T--) { // T = 100
-    if(...) //every version
-    cudaDeviceSynchronize();
-}
-gettimeofday(&end,NULL);
-fprintf(file, "%s, %d, %f\n", version.c_str(), N, time_diff(&start, &end)/T);// record time into csvfile
